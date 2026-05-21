e8dc133f6d21d19adc1a4c69700ccc19264e0aaf
natadelpilarCode <natadelpilar@gmail.com>
2026-05-21 16:38:49 -0500

    Actualización inventario con generador de reportes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,40 @@
-﻿using InventarioApp.Factories;
+﻿using InventarioApp.Infrastructure;
+using InventarioApp.Models;
+using InventarioApp.Factories;
+
+
+var productos = new List<Producto>
+{
+  ProductFactory.Crear("Laptop",1200.00m,3,CategoriaProducto.Electronica),  
+  ProductFactory.Crear("Camisa",45.00m,15,CategoriaProducto.Ropa),
+  ProductFactory.Crear("Arroz",12.00m,50,CategoriaProducto.Alimentos),
+  ProductFactory.Crear("Lámpara",35.00m,2,CategoriaProducto.Hogar),
+  ProductFactory.Crear("Balón",25.00m,8,CategoriaProducto.Deportes),
+  ProductFactory.Crear("Mesa",150.00m,4,CategoriaProducto.Muebles),
+};
+
+var generador = new GeneradorReporte(productos);
+
+Console.WriteLine(generador.GenerarResumen());
+Console.WriteLine("\n");
+
+
+Console.WriteLine(generador.GenerarReporteStockBajo());
+Console.WriteLine("\n");
+
+
+Console.WriteLine(generador.GenerarTopProductos());
+Console.WriteLine("\n");
+
+
+Console.WriteLine(generador.ExportarCsv());
+Console.WriteLine("\n");
+
+
+Console.WriteLine(generador.ExportarResumenJson());
+Console.WriteLine("\n");
+
+/*using InventarioApp.Factories;
 using InventarioApp.Models;
 using InventarioApp.Repositories;
 using InventarioApp.Infrastructure;
@@ -39,3 +75,4 @@ foreach(var p in productosCargados)
     Console.WriteLine($"ID: {p.Id}, Nombre: {p.Nombre}, Precio: {p.Precio}, Cantidad: {p.Cantidad}, Categoria: {p.Categoria}, Estado: {p.Estado}");
 }
 
+*/
